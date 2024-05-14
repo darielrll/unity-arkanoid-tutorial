76d6c8d0d6269ed76631877fb861eb57c27eb921
Dariel Rivero LLerena <leiradrll@gmail.com>
2024-05-14 00:48:35 -0400

    update: add initial script for player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    private float inputValue;
+    public float moveSpeed = 25;
+    private Vector2 direction;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
